9e42004d85050dac7c26d078336d43eec9e0a987
bradburyr1 <bradburyrp@gmail.com>
2016-03-26 21:02:29 -0400

    Update feedForwardNN.m

--- feedForwardNN.m
+++ feedForwardNN.m
@@ -1,4 +1,7 @@
 function output = feedForwardNN(Vin, Mw1, Vb1, n, Mw2, Vb2, m)
+
+%Experimental
+
 % this is a MLP, only one hidden layer (i.e. 3 layers)
 % input:   Vin = input Neurons Vector
 %          Mw1 = weights between input layer and hidden layer matrix
@@ -9,16 +12,28 @@ function output = feedForwardNN(Vin, Mw1, Vb1, n, Mw2, Vb2, m)
 %          m = number of output neurons
 
 % make sure parameters are correct
+
+%Get the input data from a file. The way its written, it requires the extra at
+%the top to be removed
+Vin = importdata('Experimental files/trainingSamp1-2.txt');
+%Reshape it into a vector
+Vin = reshape(Vin, 784, 1);
+
+n = 15;
+m = 36;
+
 if (size(Vin,1) > 1 && size(Vin,2) == 1) Vin = Vin'; end
 if (size(Vin,1) ~= 1)
   disp('input needs to be a vector');
   return;
 end
+Vb1=ones(1,15)*.1;
 if (size(Vb1,1) > 1 && size(Vb1,2) == 1) Vb1 = Vb1'; end
 if (size(Vb1,1) ~= 1)
   disp('Vb1 needs to be a vector');
   return;
 end
+Vb2=ones(1,36)*.1;
 if (size(Vb2,1) > 1 && size(Vb2,2) == 1) Vb2 = Vb2'; end
 if (size(Vb2,1) ~= 1)
   disp('Vb2 needs to be a vector');
@@ -32,10 +47,20 @@ if (length(Vb2) ~= m)
   disp('invalid Vb2');
   return;
 end
+
+%Initialize Mw1
+Mw1=zeros([784 15]);
+Mw1 = Mw1 + .1;
+
 if (sum(size(Mw1) == [length(Vin) n]) ~= 2)
   disp('invalid Mw1 matrix dimensions');
   return;
 end
+
+%Initialize Mw2
+Mw2= zeros([15 36]);
+Mw2 = Mw2 + .1;
+
 if (sum(size(Mw2) == [n m]) ~= 2)
   disp('invalid Mw2 matrix dimensions');
   return;
