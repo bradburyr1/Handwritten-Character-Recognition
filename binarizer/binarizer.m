b3b57c738be8c5c4ea2030227c6965e204109c25
Ryan Bradbury <Ryan Bradbury>
2016-04-23 23:02:59 -0400

    Added comments

--- binarizer/binarizer.m
+++ binarizer/binarizer.m
@@ -1,4 +1,9 @@
-I = imread ("H.png");
+%This program takes an image and converts it to ones and zeros.
+%Make sure you have the latest Octave version installed (or the image package, 
+%which the latest Octave version, 4.0.2, comes with). 
+%Use the command pkg load image to load the image package.
+
+I = imread ("R.png");
 
 BW = im2bw(I, .7);
 
